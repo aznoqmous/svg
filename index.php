ce2f4fb50bfa6a6590985ec2d3818814dde2a065
plandreau <plandreau@addictic.fr>
2019-06-21 15:17:08 +0200

    ++

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <script src="script.js"></script>
   </head>
   <body>
-    <svg pointer-events="none" class="leaflet-zoom-animated" width="590" height="559" viewBox="-67 -136 590 559" style="transform: translate3d(-67px, -136px, 0px);">
-      <path class="leaflet-interactive" stroke="#3388ff" stroke-opacity="1" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="#3388ff" fill-opacity="0.2" fill-rule="evenodd" d="M230 197L230 197zM200 142L201 147L203 147L206 144L216 152L217 151L219 153L225 149L230 150L234 155L235 154L234 157L236 159L237 167L233 171L232 178L230 180L230 186L232 192L234 192L235 195L233 197L231 197L231 195L229 195L229 197L223 196L225 196L224 195L225 196L215 196L216 195L214 197L222 196L216 197L209 196L208 197L212 197L204 198L188 205L187 204L187 197L189 193L183 186L177 184L180 181L178 173L180 173L183 168L180 166L180 164L186 164L186 162L183 161L185 158L184 155L181 154L181 149L188 144L189 146L192 147L196 145L196 143L200 142z"></path>
+    <svg pointer-events="none" class="leaflet-zoom-animated" width="590" height="559" viewBox="0 0 100 100">
     </svg>
+    https://developer.mozilla.org/fr/docs/Web/SVG/Tutoriel/Paths
     <script type="text/javascript">
       new SVG({
         element: document.querySelector('svg')
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 class SVG{
+
   constructor(config){
     this.config = Object.assign({}, config)
     this.init()
@@ -21,24 +22,30 @@ class SVG{
 
 
 class Path{
+
   constructor(config){
-    this.config = Object.assign({}, config)
+    this.config = Object.assign({
+      startPath: null,
+      endPath: null
+    }, config)
     this.init()
   }
+
   init(){
     if(!this.config.element) {
       console.log('Please provide a svg element')
       return delete this;
     }
     this.element = this.config.element
-    this.path = this.getPath()
-    console.log(this.path)
+    this.endPath = this.getPath()
   }
+
   getPath(){
     return this.element.getAttribute('d').split(' ')
   }
 
   setPath(){
-      this.config.element.setAttribute('d', this.paths[i].join(' '))
+      this.config.element.setAttribute('d', this.path.join(' '))
   }
+
 }
